9d8a631d668e244f9093e12a14fdabab6ceb153d
Samanta Karmaker <samonto0726@gmail.com>
2024-08-15 15:25:13 +0600

    Mongodb configuration done

--- server/src/main/java/com/example/server/config/MongoConfig.java
+++ server/src/main/java/com/example/server/config/MongoConfig.java
@@ -1,2 +1,25 @@
-package com.example.server.config;public class MongoConfig {
+package com.example.server.config;
+
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.Objects;
+
+@Configuration
+public class MongoConfig {
+    private final DotenvConfig dotenvConfig;
+
+    public MongoConfig(DotenvConfig dotenvConfig) {
+        this.dotenvConfig = dotenvConfig;
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(
+                MongoClients.create(Objects.requireNonNull(dotenvConfig.dotenv().get("MONGODB_CONNECTION_STRING"))),
+                Objects.requireNonNull(dotenvConfig.dotenv().get("MONGODB_DATABASE_NAME"))
+        );
+    }
 }
