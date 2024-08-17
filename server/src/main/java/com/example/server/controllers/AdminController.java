1fa68151cf8057842330ef43a0a6a74f3b79deee
Samanta Karmaker <samonto0726@gmail.com>
2024-08-17 22:39:45 +0600

    feat: Added endpoint to retrieve list of all users

--- server/src/main/java/com/example/server/controllers/AdminController.java
+++ server/src/main/java/com/example/server/controllers/AdminController.java
@@ -2,14 +2,33 @@ package com.example.server.controllers;
 
 import com.example.server.models.User;
 import com.example.server.models.dto.DefaultResponseDTO;
+import com.example.server.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
+
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<DefaultResponseDTO>> getUsers() {
+        List<User> allUsers = userService.getAllUsers();
+        List<DefaultResponseDTO> data = allUsers.stream()
+                .map(this::prepareResponseUser)
+                .toList();
+        return ResponseEntity.ok(data);
+    }
 
     private DefaultResponseDTO prepareResponseUser(User user) {
         return new DefaultResponseDTO(Map.of(
--- server/src/main/java/com/example/server/services/UserService.java
+++ server/src/main/java/com/example/server/services/UserService.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -76,6 +77,10 @@ public class UserService {
             throw new IllegalArgumentException("Token is expired");
         }
         return true;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
     public User blockUser(String id) {
