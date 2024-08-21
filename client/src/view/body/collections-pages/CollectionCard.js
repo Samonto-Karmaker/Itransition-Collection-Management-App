c2bf9e87eee29f97ee639ecbfc6811bd1fac6e4d
Samanta Karmaker <samonto0726@gmail.com>
2024-08-21 23:14:06 +0600

    My collection page done

--- client/src/view/body/collections-pages/CollectionCard.js
+++ client/src/view/body/collections-pages/CollectionCard.js
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
 import { Card, Button } from 'react-bootstrap';
+import { UserContext } from '../../../components/UserContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const CollectionCard = ({ collection }) => {
 
-    const {name, category, description} = collection;
+    const {userId, name, category, description} = collection;
+    const {User} = useContext(UserContext);
 
     return (
         <Card>
@@ -11,7 +16,16 @@ const CollectionCard = ({ collection }) => {
                 <Card.Subtitle className="mb-2 text-muted">{category}</Card.Subtitle>
                 <Card.Text>{description}</Card.Text>
             </Card.Body>
-            <Card.Footer>
+            <Card.Footer style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                gap: "10px",
+            }}>
+                {User && User.id === userId && (
+                    <Button variant="danger">
+                        <FontAwesomeIcon icon={faTrash} />
+                    </Button>
+                )}
                 <Button variant="primary">View Collection</Button>
             </Card.Footer>
         </Card>
