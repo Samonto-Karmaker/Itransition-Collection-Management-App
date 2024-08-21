2c0b1bb7d508f192c6651d6fad4ab689790926f1
Samanta Karmaker <samonto0726@gmail.com>
2024-08-21 23:26:32 +0600

    feat: added delete functionality

--- client/src/view/body/collections-pages/CollectionCard.js
+++ client/src/view/body/collections-pages/CollectionCard.js
@@ -1,35 +1,70 @@
-import { useContext } from 'react';
-import { Card, Button } from 'react-bootstrap';
-import { UserContext } from '../../../components/UserContext';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { useContext } from "react";
+import { Card, Button } from "react-bootstrap";
+import { UserContext } from "../../../components/UserContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { config } from "../../../constant";
 
 const CollectionCard = ({ collection }) => {
+	const { id, userId, name, category, description } = collection;
+	const { User } = useContext(UserContext);
+	const token = localStorage.getItem("token");
 
-    const {userId, name, category, description} = collection;
-    const {User} = useContext(UserContext);
+	const handleDelete = async (collectionId) => {
+		if (
+			window.confirm("Are you sure you want to delete this collection?")
+		) {
+            console.log(collection);
+			const url =
+				config.API_URL + "/api/collections/delete/" + collectionId;
+			try {
+				const response = await fetch(url, {
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${token}`,
+					},
+				});
+				const result = await response.text();
+                if (response.ok) {
+                    window.alert("Collection deleted successfully");
+                    window.location.reload();
+                    return;
+                } else {
+                    window.alert(result);
+                }
+			} catch (error) {
+				console.error(error);
+				window.alert("Failed to delete collection");
+			}
+		}
+	};
 
-    return (
-        <Card>
-            <Card.Body>
-                <Card.Title>{name}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{category}</Card.Subtitle>
-                <Card.Text>{description}</Card.Text>
-            </Card.Body>
-            <Card.Footer style={{
-                display: "flex",
-                justifyContent: "flex-end",
-                gap: "10px",
-            }}>
-                {User && User.id === userId && (
-                    <Button variant="danger">
-                        <FontAwesomeIcon icon={faTrash} />
-                    </Button>
-                )}
-                <Button variant="primary">View Collection</Button>
-            </Card.Footer>
-        </Card>
-    )
+	return (
+		<Card>
+			<Card.Body>
+				<Card.Title>{name}</Card.Title>
+				<Card.Subtitle className="mb-2 text-muted">
+					{category}
+				</Card.Subtitle>
+				<Card.Text>{description}</Card.Text>
+			</Card.Body>
+			<Card.Footer
+				style={{
+					display: "flex",
+					justifyContent: "flex-end",
+					gap: "10px",
+				}}
+			>
+				{User && User.id === userId && (
+					<Button variant="danger" onClick={() => handleDelete(id)}>
+						<FontAwesomeIcon icon={faTrash} />
+					</Button>
+				)}
+				<Button variant="primary">View Collection</Button>
+			</Card.Footer>
+		</Card>
+	);
 };
 
 export default CollectionCard;
