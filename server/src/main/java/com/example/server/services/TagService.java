38caa5a415a9377999d21a7879f9eae584318994
Samanta Karmaker <samonto0726@gmail.com>
2024-08-23 12:16:40 +0600

    Added business logic and endpoint to get all tags

---
+++ server/src/main/java/com/example/server/services/TagService.java
@@ -0,0 +1,21 @@
+package com.example.server.services;
+
+import com.example.server.models.Tag;
+import com.example.server.repositories.TagRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TagService {
+
+    private final TagRepository tagRepository;
+
+    public TagService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    public List<Tag> getAllTags() {
+        return tagRepository.findAll();
+    }
+}
